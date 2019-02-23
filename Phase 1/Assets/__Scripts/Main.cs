4773cde819d8426a3ce3008218c4c3e49961534f
zmascian <zmascian@uwo.ca>
2019-02-23 00:19:23 -0500

    Main Script

---
+++ Phase 1/Assets/__Scripts/Main.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public static Main S;
+
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond;
+    public float enemyDefaultPadding;
+
+    private BoundsCheck bndCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        S = this;
+        //Set bndCheck to reference the BoundsCheck component on this gameObject 
+        bndCheck = GetComponent<BoundsCheck>();
+        //Invoke SpawnEnemy() once (in 2 seconds, based on default values)
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+
+    }
+
+    public void SpawnEnemy() {
+        //Pick a random enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        //Position enemy above the screen with random x position
+        float enemyPadding = enemyDefaultPadding;
+        if(go.GetComponent<BoundsCheck>() != null){
+            enemyPadding = Mathf.Abs(go.GetComponent<BoundsCheck>().raduis);
+        }
+
+        //Set the initial position for this spawned enemy
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyPadding;
+        float xMax = bndCheck.camWidth - enemyPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyPadding;
+        go.transform.position = pos;
+
+        //Invoke SpawnEnemy() again
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+}
