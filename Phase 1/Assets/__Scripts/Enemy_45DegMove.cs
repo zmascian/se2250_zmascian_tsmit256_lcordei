e9538dfc1a67e034bb2561330e4b040ea396328b
Trevor Smith <tsmit256@uwo.ca>
2019-02-24 16:42:44 -0500

    Adding Move Scripts

---
+++ Phase 1/Assets/__Scripts/Enemy_45DegMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_45DegMove : Enemy
+{
+    public override void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
---
+++ Phase 1/Assets/__Scripts/Enemy_CosMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_CosMove : Enemy
+{
+    public override void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
