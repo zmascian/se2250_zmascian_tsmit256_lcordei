6d4f65052e13737ebaf13028368ba10e6ae3bb1d
Trevor Smith <tsmit256@uwo.ca>
2019-02-24 17:24:32 -0500

    Update Enemy_2

--- Phase 1/Assets/__Scripts/Enemy_1.cs
+++ Phase 1/Assets/__Scripts/Enemy_1.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_45DegMove : Enemy
+public class Enemy_1 : Enemy
 {
     public override void Move()
     {
+
         Vector3 tempPos = pos;
+        if (Random.Range(1,2) == 2)
+        {
+            tempPos.x -= speed * Time.deltaTime;
+        }
+        else
+        {
+            tempPos.x += speed * Time.deltaTime;
+        }
         tempPos.y -= speed * Time.deltaTime;
         pos = tempPos;
     }
