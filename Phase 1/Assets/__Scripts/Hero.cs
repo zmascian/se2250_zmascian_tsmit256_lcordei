d9c2361620b697fc664d979d452eb7b86cb4e8d7
Trevor Smith <tsmit256@uwo.ca>
2019-03-21 23:18:20 -0400

    deleting tempFire

--- Phase 1/Assets/__Scripts/Hero.cs
+++ Phase 1/Assets/__Scripts/Hero.cs
@@ -35,7 +35,7 @@ public class Hero : MonoBehaviour
         {
             Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
         }
-        fireDelegate += TempFire;
+        
     }
 
     // Update is called once per frame
@@ -58,18 +58,6 @@ public class Hero : MonoBehaviour
         {
             fireDelegate();
         }
-    }
-
-    void TempFire()
-    {
-        GameObject projGO = Instantiate<GameObject>(projectilePrefab);
-        projGO.transform.position = transform.position;
-        Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
-
-        Projectile proj = projGO.GetComponent<Projectile>();
-        proj.type = WeaponType.blaster;
-        float tSpeed = Main.GetWeaponDefinition(proj.type).velocity;
-        rigidB.velocity = Vector3.up * tSpeed;
     }
 
     void OnTriggerEnter(Collider other)
